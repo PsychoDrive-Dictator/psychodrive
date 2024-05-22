0cf278e1e2d8405cfb04279ea1c5410c82e26902
Dictator <dictator@fastmail.com>
2024-05-21 20:13:44 -0700

    more analysis toy stuff

--- analysis_scripts/test.py
+++ analysis_scripts/test.py
@@ -58,18 +58,27 @@ def checkHitParamHitStun(hitParam, left, right, expectedDiff, description):
 for char in characters:
     hitsJson = json.load(open(dataPath + char + "_hit.json"))
     movesJson = json.load(open(dataPath + char + "_moves.json"))
-    for hitID in hitsJson:
-        hitParam = hitsJson[hitID]["param"]
-        # check movetime vs hitstun
-        for param in hitParam:
-            if hitParam[param]["HitStun"] != hitParam[param]["MoveTime"]:
-                print("movetime != hitstun")
-        # check burnout block is -4 compared to normal block
-        checkHitParamHitStun(hitParam, "04", "16", -4, "burnout stand block vs stand block")
-        checkHitParamHitStun(hitParam, "16", "17", 0, "stand block vs crouch block")
-        checkHitParamHitStun(hitParam, "04", "05", 0, "burnout stand block vs burnout crouch block")
-        checkHitParamHitStun(hitParam, "00", "01", 0, "stand hit vs crouch hit")
-        checkHitParamHitStun(hitParam, "08", "09", 0, "stand counter vs crouch counter")
-        checkHitParamHitStun(hitParam, "12", "13", 0, "stand PC vs crouch PC")
-        checkHitParamHitStun(hitParam, "00", "08", 2, "stand hit vs stand counter")
-        checkHitParamHitStun(hitParam, "00", "12", 4, "stand hit vs stand PC")
+    # for hitID in hitsJson:
+    #     hitParam = hitsJson[hitID]["param"]
+    #     # check movetime vs hitstun
+    #     for param in hitParam:
+    #         if hitParam[param]["HitStun"] != hitParam[param]["MoveTime"]:
+    #             print("movetime != hitstun")
+    #     # check burnout block is -4 compared to normal block
+    #     checkHitParamHitStun(hitParam, "04", "16", -4, "burnout stand block vs stand block")
+    #     checkHitParamHitStun(hitParam, "16", "17", 0, "stand block vs crouch block")
+    #     checkHitParamHitStun(hitParam, "04", "05", 0, "burnout stand block vs burnout crouch block")
+    #     checkHitParamHitStun(hitParam, "00", "01", 0, "stand hit vs crouch hit")
+    #     checkHitParamHitStun(hitParam, "08", "09", 0, "stand counter vs crouch counter")
+    #     checkHitParamHitStun(hitParam, "12", "13", 0, "stand PC vs crouch PC")
+    #     checkHitParamHitStun(hitParam, "00", "08", 2, "stand hit vs stand counter")
+    #     checkHitParamHitStun(hitParam, "00", "12", 4, "stand hit vs stand PC")
+    for moveID in movesJson:
+        move = movesJson[moveID]
+        if "AttackCollisionKey" in move:
+            for key in move["AttackCollisionKey"]:
+                if isinstance(move["AttackCollisionKey"][key], dict) and "CollisionType" in move["AttackCollisionKey"][key]:
+                    attackKey = move["AttackCollisionKey"][key]
+                    if attackKey["KindFlag"] & 67108864:
+                        print(char + " " + moveID + " type " + str(attackKey["CollisionType"]) +
+                              " hit " + str(attackKey["HitID"]) + " frames " + str(attackKey["_StartFrame"]) + "-" + str(attackKey["_EndFrame"]))
