13d0a3502933e7e54ef0f914f2c94780f46eb600
Dictator <dictator@fastmail.com>
2024-10-12 13:12:01 -0700

    add and show version from git tag
    so we can use it when making a dist package less manually

--- render.cpp
+++ render.cpp
@@ -10,6 +10,9 @@
 #include "ui.hpp"
 #include "guy.hpp"
 
+#define STRINGIZE(x) #x
+#define STRINGIZE_VALUE_OF(x) STRINGIZE(x)
+
 SDL_Window* window = nullptr;
 SDL_GLContext gl_context;
 
@@ -354,7 +357,8 @@ SDL_Window* initWindowRender()
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    window = SDL_CreateWindow("Psycho Drive", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, window_flags);
+    std::string strWindowTitle = "Psycho Drive " + std::string(STRINGIZE_VALUE_OF(PROJECT_VERSION));
+    window = SDL_CreateWindow(strWindowTitle.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, window_flags);
     if (window == nullptr)
     {
         printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
