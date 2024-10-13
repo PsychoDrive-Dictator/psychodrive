1eec2221c68dccbb053082128fcab489f0a79fe0
Dictator <dictator@fastmail.com>
2024-10-12 20:23:36 -0700

    dynamic async char loading for emscripten
    only download the data for the chars we're loading, split files per character

--- meson_package_data.sh
+++ meson_package_data.sh
@@ -2,13 +2,17 @@
 set -e
 
 rsync -avx "$MESON_SOURCE_ROOT"/data "$1"/
-for chardir in $(find "$1"/data/chars -maxdepth 1 -type d); do
+for chardir in $(find "$1"/data/chars -maxdepth 1 -mindepth 1 -type d); do
     char=$(basename "$chardir")
     zip -j "$chardir"/"$char".zip "$chardir"/*.json
     rm "$chardir"/*.json
+    if [[ $2 == "emscripten" ]]; then
+        /usr/lib/emsdk/upstream/emscripten/tools/file_packager "$1"/psychodrive_char_"$char".data --js-output="$1"/psychodrive_char_"$char".js --embed "$1"/data/chars/"$char"@./data/chars/"$char"
+    fi
 done
 
 if [[ $2 == "emscripten" ]]; then
-    /usr/lib/emsdk/upstream/emscripten/tools/file_packager "$1"/psychodrive_files.data --js-output="$1"/psychodrive_files.js --preload "$1"/data@./data
-    rm -r "$1"/data
+    /usr/lib/emsdk/upstream/emscripten/tools/file_packager "$1"/psychodrive_files.data --js-output="$1"/psychodrive_files.js --exclude \*chars\* --embed "$1"/data@./data
 fi
+
+rm -r "$1"/data
