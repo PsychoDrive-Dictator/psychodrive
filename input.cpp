6859a6d01abb8939656095c37c95df68bbae3b71
Dictator <dictator@fastmail.com>
2024-12-29 01:50:37 -0800

    first stab at touch controls - need better scaling for different aspects
    fix a bug where box inner was ignored, can now be specified properly
    
    super simple stab at mobile haptics, for direction feedback and hitstop

--- input.cpp
+++ input.cpp
@@ -1,11 +1,127 @@
 #include <SDL.h>
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten/html5.h>
+#endif
+
 #include "input.hpp"
 #include "main.hpp"
 #include "ui.hpp"
 #include "render.hpp"
 
 std::map<int, int> currentInputMap;
+
+struct FingerState {
+    float x;
+    float y;
+};
+
+SDL_FingerID directionFinger = -1;
+std::map<SDL_FingerID, FingerState> mapFingerPositions;
+
+class TouchButton {
+public:
+    TouchButton(float x, float y, float yOffset, float radius, int inputSlot, int input) :
+        x(x), y(y), yOffset(yOffset), radius(radius), inputSlot(inputSlot), input(input) { }
+
+    void CheckInput(std::map<SDL_FingerID, FingerState> &mapState) {
+        bool foundTouch = false;
+        for (auto & fingerState: mapState) {
+            FingerState &state = fingerState.second;
+            if (state.x >= x - radius && state.x <= x+radius && state.y >= y+(yOffset-radius)/aspect && state.y <= y+(yOffset+radius)/aspect)
+                foundTouch = true;
+        }
+        if (foundTouch) {
+            currentInputMap[inputSlot] |= input;
+            if (input > 8 && active == false)
+                currentInputMap[inputSlot] |= input << 6;
+            active = true;
+        } else {
+            currentInputMap[inputSlot] &= ~input;
+            active = false;
+        }
+    }
+
+    void Render(int winX, int winY) {
+        aspect = (float)winY / (float)winX;
+        drawBox( (x - radius) * winX, y * winY + (yOffset - radius) * winY / aspect, radius * 2.0 * winX, radius * 2.0 * winY / aspect, 1.0, 1.0,1.0,1.0, active ? 0.25 : 0.1);
+    }
+
+private:
+    float x;
+    float y;
+    float yOffset;
+    float radius;
+    int inputSlot;
+    int input;
+    bool active = false;
+    float aspect = 1.0;
+};
+
+class TouchPad {
+public:
+    TouchPad(float x, float y, float innerRadius, int inputSlot) :
+        x(x), y(y), innerRadius(innerRadius), inputSlot(inputSlot) { }
+
+    void CheckInput(FingerState state) {
+#ifdef __EMSCRIPTEN__
+        int oldInput = currentInputMap[inputSlot];
+#endif
+        if (state.x >= 0.0f && state.x >= x + innerRadius) {
+            currentInputMap[inputSlot] |= FORWARD;
+        } else {
+            currentInputMap[inputSlot] &= ~FORWARD;
+        }
+        if (state.x >= 0.0f && state.x <= x - innerRadius) {
+            currentInputMap[inputSlot] |= BACK;
+        } else {
+            currentInputMap[inputSlot] &= ~BACK;
+        }
+        if (state.y >= 0.0f && state.y <= y - innerRadius/aspect) {
+            currentInputMap[inputSlot] |= UP;
+        } else {
+            currentInputMap[inputSlot] &= ~UP;
+        }
+        if (state.y >= 0.0f && state.y >= y + innerRadius/aspect) {
+            currentInputMap[inputSlot] |= DOWN;
+        } else {
+            currentInputMap[inputSlot] &= ~DOWN;
+        }
+
+#ifdef __EMSCRIPTEN__
+        if (currentInputMap[inputSlot] != oldInput)
+            emscripten_vibrate(5);
+#endif
+    }
+
+    void Render(int winX, int winY) {
+        aspect = (float)winY / (float)winX;
+        drawBox( (x - innerRadius) * winX, y * winY - innerRadius * winY / aspect, innerRadius * 2.0 * winX, innerRadius * 2.0 * winY / aspect, 1.0, 1.0,1.0,1.0, 0.1);
+        float xOffset = 0.0f, yOffset = 0.0f;
+        if (currentInputMap[inputSlot] & FORWARD) {
+            xOffset = innerRadius / 2.0;
+        }
+        if (currentInputMap[inputSlot] & BACK) {
+            xOffset = -innerRadius / 2.0;
+        }
+        if (currentInputMap[inputSlot] & UP) {
+            yOffset = innerRadius / 2.0;
+        }
+        if (currentInputMap[inputSlot] & DOWN) {
+            yOffset = -innerRadius / 2.0;
+        }
+        drawBox( (x - innerRadius / 2.0 + xOffset) * winX, y * winY - (innerRadius / 2.0 + yOffset) * winY / aspect, innerRadius * winX, innerRadius * winY / aspect, 1.0, 1.0,1.0,1.0, 0.1);
+    }
+private:
+    float x;
+    float y;
+    float innerRadius;
+    int inputSlot;
+    float aspect = 1.0;
+};
+
+std::vector<TouchButton> vecTouchButtons;
+std::vector<TouchPad> vecTouchPads;
 
 int addPressBits(int curInput, int prevInput)
 {
@@ -24,13 +140,14 @@ int addPressBits(int curInput, int prevInput)
     return retInput;
 }
 
-void updateInputs(void)
+void updateInputs(int sizeX, int sizeY)
 {
     // clear new press bits
     for (auto i : currentInputMap) {
         currentInputMap[i.first] &= ~(LP_pressed+MP_pressed+HP_pressed+LK_pressed+MK_pressed+HK_pressed);
     }
     SDL_Event event;
+    bool directionFingerUp = false;
     while (SDL_PollEvent(&event))
     {
         ImGui_ImplSDL2_ProcessEvent(&event);
@@ -296,6 +413,26 @@ void updateInputs(void)
                     break;
             }
         }
+        if (event.type == SDL_FINGERDOWN || event.type == SDL_FINGERMOTION || event.type == SDL_FINGERUP)
+        {
+            if (event.type == SDL_FINGERUP) {
+                event.tfinger.x = -1.0f;
+                event.tfinger.y = -1.0f;
+
+                if (event.tfinger.fingerId == directionFinger) {
+                    directionFingerUp = true;
+                }
+            }
+            FingerState state = { event.tfinger.x, event.tfinger.y };
+            mapFingerPositions[event.tfinger.fingerId] = state;
+            //log("stickyfingerz " + std::to_string(event.tfinger.fingerId) + " " + std::to_string(event.tfinger.x) + " " + std::to_string(event.tfinger.y));
+
+            if (event.type == SDL_FINGERDOWN && event.tfinger.y > 0.5 && event.tfinger.x < 0.5) {
+                directionFinger = event.tfinger.fingerId;
+                vecTouchPads.push_back(TouchPad(event.tfinger.x, event.tfinger.y, 25.0 / sizeX, keyboardID));
+            }
+        }
+        static bool mouseLeftPressed = false;
         if (!ImGui::GetIO().WantCaptureMouse)
         {
             if(event.type == SDL_MOUSEWHEEL)
@@ -307,11 +444,10 @@ void updateInputs(void)
                     zoom -= 50.0;
                 }
             }
-            static bool mouseLeftPressed = false;
             if (event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEBUTTONUP)
             {
                 if (event.button.button == SDL_BUTTON_LEFT) {
-                    mouseLeftPressed = event.button.state == SDL_PRESSED;
+                    mouseLeftPressed = event.button.state == SDL_PRESSED && event.button.y < sizeY / 2;
                 }
             }
             if (event.type == SDL_MOUSEMOTION)
@@ -321,6 +457,43 @@ void updateInputs(void)
                     translateY += event.motion.yrel;
                 }
             }
+        } else {
+            mouseLeftPressed = false;
         }
-   }
+    }
+
+    // resolve finger positions into touch button input
+    for (auto & button : vecTouchButtons) {
+        button.CheckInput(mapFingerPositions);
+    }
+    if (directionFinger != -1) {
+        for (auto & pad : vecTouchPads) {
+            pad.CheckInput(mapFingerPositions[directionFinger]);
+        }
+    }
+    // only remove pad after checking input to remove the direction input
+    if (directionFingerUp) {
+        directionFinger = -1;
+        vecTouchPads.clear();
+    }
+}
+
+void initTouchControls(void)
+{
+    vecTouchButtons.push_back(TouchButton(0.6, 0.70, 0.05, 0.06, keyboardID, LP));
+    vecTouchButtons.push_back(TouchButton(0.75, 0.70, 0.0, 0.06, keyboardID, MP));
+    vecTouchButtons.push_back(TouchButton(0.90, 0.70, 0.0, 0.06, keyboardID, HP));
+    vecTouchButtons.push_back(TouchButton(0.6, 0.70, 0.20, 0.06, keyboardID, LK));
+    vecTouchButtons.push_back(TouchButton(0.75, 0.70, 0.15, 0.06, keyboardID, MK));
+    vecTouchButtons.push_back(TouchButton(0.90, 0.70, 0.15, 0.06, keyboardID, HK));
+}
+
+void renderTouchControls(int sizeX, int sizeY)
+{
+    for (auto & button : vecTouchButtons) {
+        button.Render(sizeX, sizeY);
+    }
+    for (auto & pad : vecTouchPads) {
+        pad.Render(sizeX, sizeY);
+    }
 }
