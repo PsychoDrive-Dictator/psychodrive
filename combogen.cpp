5b9bec4715429ad4def17e17692916b3041dc53b
Dictator <dictator@fastmail.com>
2025-10-13 00:31:16 -0700

    speed up routing by not resimulating every route from the beginning
    snapshot from branch point

--- combogen.cpp
+++ combogen.cpp
@@ -10,14 +10,21 @@
 
 struct ComboRoute {
     std::map<int, std::pair<int, int>> timelineTriggers;
+    bool startedCombo = false;
     int simFrameProgress = 0;
     int guyFrameProgress = 0;
     int damage = 0;
     int lastFrameDamage = 0;
+    Guy guys[2];
+};
+
+struct DoneRoute {
+    std::map<int, std::pair<int, int>> timelineTriggers;
+    int damage = 0;
 };
 
 struct DamageSort {
-    bool operator()(ComboRoute const& lhs, ComboRoute const& rhs) const {
+    bool operator()(DoneRoute const& lhs, DoneRoute const& rhs) const {
         if (lhs.damage == rhs.damage) {
             return lhs.timelineTriggers < rhs.timelineTriggers;
         }
@@ -26,7 +33,7 @@ struct DamageSort {
 };
 
 std::deque<ComboRoute> pendingRoutes;
-std::set<ComboRoute, DamageSort> doneRoutes;
+std::set<DoneRoute, DamageSort> doneRoutes;
 
 void findCombos(void)
 {
@@ -34,27 +41,27 @@ void findCombos(void)
 
     ComboRoute currentRoute;
 
-    while (true) {
-        currentRoute.damage = 0;
-        currentRoute.lastFrameDamage = 0;
-        Simulation *pSim = new Simulation;
-        bool startedCombo = false;
-        pSim->CreateGuy(*guys[0]->getName(), guys[0]->getVersion(), guys[0]->getPosX(), guys[0]->getPosY(), guys[0]->getDirection(), guys[0]->getColor());
-        pSim->CreateGuy(*guys[1]->getName(), guys[1]->getVersion(), guys[1]->getPosX(), guys[1]->getPosY(), guys[1]->getDirection(), guys[1]->getColor());
-        *pSim->simGuys[0] = *guys[0];
-        *pSim->simGuys[1] = *guys[1];
-        // pSim->simGuys[0]->setSim(pSim);
-        // pSim->simGuys[1]->setSim(pSim);
-        // todo not really facsimiles.. don't leak movelist
-        pSim->simGuys[0]->facSimile = true;
-        pSim->simGuys[1]->facSimile = true;
-        pSim->simGuys[0]->setOpponent(pSim->simGuys[1]);
-        pSim->simGuys[1]->setOpponent(pSim->simGuys[0]);
-        if (pSim->simGuys[1]->getAttacker() != nullptr) {
-            pSim->simGuys[1]->setAttacker(pSim->simGuys[0]);
-        }
+    Simulation *pSim = new Simulation;
+    pSim->CreateGuy(*guys[0]->getName(), guys[0]->getVersion(), guys[0]->getPosX(), guys[0]->getPosY(), guys[0]->getDirection(), guys[0]->getColor());
+    pSim->CreateGuy(*guys[1]->getName(), guys[1]->getVersion(), guys[1]->getPosX(), guys[1]->getPosY(), guys[1]->getDirection(), guys[1]->getColor());
+    *pSim->simGuys[0] = *guys[0];
+    *pSim->simGuys[1] = *guys[1];
+    // pSim->simGuys[0]->setSim(pSim);
+    // pSim->simGuys[1]->setSim(pSim);
+    // todo not really facsimiles.. don't leak movelist
+    pSim->simGuys[0]->facSimile = true;
+    pSim->simGuys[1]->facSimile = true;
+    pSim->simGuys[0]->setOpponent(pSim->simGuys[1]);
+    pSim->simGuys[1]->setOpponent(pSim->simGuys[0]);
+    if (pSim->simGuys[1]->getAttacker() != nullptr) {
+        pSim->simGuys[1]->setAttacker(pSim->simGuys[0]);
+    }
 
+    while (true) {
         while (true) {
+            currentRoute.guys[0] = *pSim->simGuys[0];
+            currentRoute.guys[1] = *pSim->simGuys[1];
+
             auto &forcedTrigger = pSim->simGuys[0]->getForcedTrigger();
             if (currentRoute.timelineTriggers.find(pSim->frameCounter+1) != currentRoute.timelineTriggers.end()) {
                 forcedTrigger = currentRoute.timelineTriggers[pSim->frameCounter+1];
@@ -64,39 +71,46 @@ void findCombos(void)
             pSim->RunFrame();
             pSim->AdvanceFrame();
 
+            if (currentRoute.guyFrameProgress < pSim->simGuys[0]->getCurrentFrame() &&
+                currentRoute.simFrameProgress < pSim->frameCounter) {
+                currentRoute.simFrameProgress = pSim->frameCounter;
+                for (auto &frameTrigger : pSim->simGuys[0]->getFrameTriggers()) {
+                    pendingRoutes.push_front(currentRoute);
+                    pendingRoutes.front().timelineTriggers[pSim->frameCounter] = frameTrigger;
+                }
+            }
+
             if (pSim->simGuys[1]->getComboDamage() > currentRoute.damage) {
                 currentRoute.damage = pSim->simGuys[1]->getComboDamage();
                 currentRoute.lastFrameDamage = pSim->frameCounter;
             }
 
-            if (currentRoute.guyFrameProgress < pSim->simGuys[0]->getCurrentFrame() &&
-                currentRoute.simFrameProgress < pSim->frameCounter) {
-                currentRoute.simFrameProgress = pSim->frameCounter;
-                for (auto &frameTrigger : pSim->simGuys[0]->getFrameTriggers()) {
-                    pendingRoutes.push_back(currentRoute);
-                    pendingRoutes.back().timelineTriggers[pSim->frameCounter] = frameTrigger;
-                }
-            }
-
-            currentRoute.guyFrameProgress = pSim->simGuys[0]->getCurrentFrame();
-
             if (pSim->simGuys[1]->getComboHits()) {
-                startedCombo = true;
+                currentRoute.startedCombo = true;
             }
 
-            if (pSim->frameCounter == 2000 || (!pSim->simGuys[1]->getComboHits() && startedCombo) || pSim->simGuys[1]->getIsDown() || pSim->simGuys[0]->canAct()) {
+            if (pSim->frameCounter == 2000 || (!pSim->simGuys[1]->getComboHits() && currentRoute.startedCombo) || pSim->simGuys[1]->getIsDown() || pSim->simGuys[0]->canAct()) {
                 //pSim->Log("framecount " + std::to_string(pSim->frameCounter));
                 break;
             }
-        }
 
-        delete pSim;
+            currentRoute.guyFrameProgress = pSim->simGuys[0]->getCurrentFrame();
+        }
 
         std::erase_if(currentRoute.timelineTriggers, [=](const auto& item) {
             return item.first > currentRoute.lastFrameDamage;
         });
 
-        doneRoutes.insert(currentRoute);
+        DoneRoute doneRoute;
+        doneRoute.timelineTriggers = currentRoute.timelineTriggers;
+        doneRoute.damage = currentRoute.damage;
+        std::string logEntry = std::to_string(doneRoute.damage) + " damage: ";
+        for ( auto &trigger : doneRoute.timelineTriggers) {
+            logEntry += std::to_string(trigger.first) + " " + guys[0]->getActionName(trigger.second.first) + " ";
+        }
+        log(logEntry);
+        fprintf(stderr, "%s\n", logEntry.c_str());
+        doneRoutes.insert(doneRoute);
 
         if (!pendingRoutes.size()) {
             break;
@@ -104,7 +118,13 @@ void findCombos(void)
 
         currentRoute = pendingRoutes.front();
         pendingRoutes.pop_front();
+
+        *pSim->simGuys[0] = currentRoute.guys[0];
+        *pSim->simGuys[1] = currentRoute.guys[1];
+        pSim->frameCounter = currentRoute.simFrameProgress-1;
     }
+
+    delete pSim;
 
     for (auto &route : doneRoutes) {
         std::string logEntry = std::to_string(route.damage) + " damage: ";
@@ -119,4 +139,6 @@ void findCombos(void)
     auto logEntry = std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0f) + "s elapsed";
     log(logEntry);
     fprintf(stderr, "%s\n", logEntry.c_str());
+
+    doneRoutes.clear();
 }
