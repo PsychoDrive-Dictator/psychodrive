1cf7dd2fe490e0cd19ea7a07d17283e70474ad96
Dictator <dictator@fastmail.com>
2026-03-25 00:20:32 -0700

    dust off non-emscripten build path, tidy up for a release

--- meson_package_data.sh
+++ meson_package_data.sh
@@ -2,20 +2,29 @@
 set -e
 
 cd "$MESON_SOURCE_ROOT"
-python3 "$MESON_SOURCE_ROOT"/misc/scripts/cook_all_chars.py "$1"/ "$MESON_SOURCE_ROOT"/psychodrive
+
+cookpath=
+if [[ $2 == "emscripten" ]]; then
+    cookpath="$1"/
+else
+    cookpath="$1"/data/cooked
+fi
+mkdir -p "$cookpath"
+python3 "$MESON_SOURCE_ROOT"/misc/scripts/cook_all_chars.py "$cookpath"/ "$MESON_SOURCE_ROOT"/psychodrive
 
 rsync -avx --exclude='chars' --exclude='cooked' "$MESON_SOURCE_ROOT"/data "$1"/
 
 if [[ $2 == "emscripten" ]]; then
     /usr/lib/emsdk/upstream/emscripten/tools/file_packager "$1"/psychodrive_files.data --js-output="$1"/psychodrive_files.js --exclude \*cooked\* --preload "$1"/data@./data
+    rm -r "$1"/data
 
     version=$(basename "$1")
     version=${version#psychodrive-}
     sed -i -e "s/version=/version=$version/g" "$1"/psychodrive.html
+
+    latest_path="$(dirname "$1")"/psychodrive-latest
+    rm -f "$latest_path"
+    ln -s "$1" "$latest_path"
 fi
 
-rm -r "$1"/data
 
-latest_path="$(dirname "$1")"/psychodrive-latest
-rm -f "$latest_path"
-ln -s "$1" "$latest_path"
